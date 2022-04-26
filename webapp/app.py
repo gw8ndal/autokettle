a51b6b82e9485dfba814c2c0cfbbf11c48a1933c
Gwendal Troadec <gw8ndal@gmail.com>
2022-04-26 21:04:36 +0200

    temperature graph works

--- webapp/app.py
+++ webapp/app.py
@@ -21,16 +21,11 @@ def data():
         
         data.raspy_pin = data.req['GPIO_pin']
         data.kettle_temp = data.req['kettle_temp']
-
-        if 'filesave_toggle' in data.req: # Check if the box was checked
-            userdata_file = open('userdata.txt', 'w+') # Open the userdata file
-            userdata_file.writelines([f'{data.raspy_pin},{data.kettle_temp}']) # Write content to the file
-            userdata_file.close()
-        else:
-            try:
-                os.remove('userdata.txt')
-            except:
-                pass
+        
+        userdata_file = open('userdata.txt', 'w+') # Open the userdata file
+        userdata_file.writelines([f'{data.raspy_pin},{data.kettle_temp}']) # Write content to the file
+        userdata_file.close()
+        
         return redirect('/')
 
 @app.route('/heat', methods=['GET', 'POST'])
@@ -57,8 +52,10 @@ def heat():
 
 @sock.route('/graph')
 def graph(sock):
+    # Placeholder for the actual temperature probe
+    import random
     data = 30
-    while data < 100:
-        data += 2
+    while True:
         sock.send(data)
+        data = random.randint(30, 99)
         time.sleep(1)
--- webapp/static/js/chart.js
+++
@@ -1,1 +0,0 @@
-console.log('zebi ca marche')
